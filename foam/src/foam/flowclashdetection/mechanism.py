cdad8e6572e2826caea382ec40fb8b1a759e9074
Vasileios Kotronis <vasileios.kotronis@tik.ee.ethz.ch>
2012-10-18 15:44:50 +0200

    Started working on flowspace collision detection engine

---
+++ foam/src/foam/flowclashdetection/mechanism.py
@@ -0,0 +1,32 @@
+#Vasileios: implementation of the flowspace collision detection engine
+
+from foam.flowvisor import FSAllocation
+from foam.lib import FlowSpec
+
+class MACTree(object):
+	def __init__(self):
+		
+class ETherTypeTree(object):
+	def __init__(self):
+
+class VLANTree(object):
+	def __init__(self):
+
+class IPSubnetTree(object):
+	def __init__(self):
+	
+class NWProtoTree(object):
+	def __init__(self):
+	
+class TPPortTree(object):
+	def __init__(self):
+	
+class DataPathTree(object):
+	def __init__(self):
+	
+def getAllocatedFlowSpace():
+	current_allocation = FSAllocation()
+	for fs in current_allocation.getFlowspecs:
+		
+		
+
