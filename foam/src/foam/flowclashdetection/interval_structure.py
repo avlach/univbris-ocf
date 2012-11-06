4711351b55aa8926570885f48e8f2ee6b058577a
Vasileios Kotronis <vasileios.kotronis@tik.ee.ethz.ch>
2012-11-06 17:08:33 +0100

    More work on interval trees for collision detection plus uploaded some legacy work on legacy optin manager: translation of Expedient data to ofv3 rspecs using lxml

--- foam/src/foam/flowclashdetection/interval_structure.py
+++ foam/src/foam/flowclashdetection/interval_structure.py
@@ -1,3 +1,4 @@
+#construct an interval tree structure
 #author: Vasileios Kotronis
 #based on:
 #-- http://en.wikipedia.org/wiki/Interval_tree --> main source #1
@@ -15,12 +16,13 @@ class IValNode(object):
 	def __init__(self, low_end, high_end, value):
 		self.low_end = low_end
 		self.high_end = high_end
+		self.max_high = 0
 		self.value = value
 		self.left = None
 		self.right = None
-		self.max_high = 0
+		self.parent = None
 		
-
+	
 class IValTree(object):
 	
 	def __init__(self):
@@ -33,27 +35,50 @@ class IValTree(object):
 			root.max_high = max(root.max_high, stop)
 			if start <= root.low_end:
 				root.left = self.addIVal(root.left, start, stop, value)
+				root.left.parent = root
 			else:
 				root.right = self.addIVal(root.right, start, stop, value)
+				root.right.parent = root
 		return root
 	
-	#work needs to be done in removal...
-	def remIVal(self, parent, root, value):
-		if root == None:
-			pass
-		else:
-			if root.value == value:
-				if root is parent.left:
-
-					parent.left = root.left
-					
+	def remIVal(self, root, start, stop, value):
+		iValList = self.findOverlapIVal(root, start, stop,  [])
+		for i in range(len(iValList)):
+			if iValList[i].value == value:
+				break
+		node_to_remove = iValList[i]
+		if node_to_remove is None:
+			print "The current interval is non-existent!"
+		else if node_to_remove.parent is None: #This is the current root
+			if node_to_remove.left is None:
+				if node_to_remove.right is None:
+					root = None
 				else:
-					parent.right = root.right
+					node_to_remove.right.parent = None
+					root = node_to_remove.right
+			else:
+				if node_to_remove.right is None:
+					node_to_remove.left.parent = None
+					root = node_to_remove.left
+				else:
+					
+		else:
+			if node_to_remove.low_end <= node_to_remove.parent.low_end: #node is at the left
+				node_to_remove.parent.left = node_to_remove.left
+				rec_node = node_to_remove.parent
+				while rec_node != None:
+					rec_node = rec_node.parent
+					rec_node.max_high = max(rec_node.left.max_high, rec_node.right.max_high)
+					if rec_node.left is None or rec_node.right is None
+			else: #node is at the right
+				node_to_remove.parent.right = node_to_remove.right
 				
-			self.remIVal(root, root.left, value)
-			self.remIVal(root, root.right, value)
+		node_to_remove = None		
 		
-		
+		return root
+			
+		else:
+			
 	def findOverlapIVal(self, root, start, stop, clashList):
 		cList = clashList
 		if root != None:
@@ -85,7 +110,7 @@ class IValTree(object):
 		if root == None:
 			pass
 		else:
-			print "[" + root.low_end + "-" + root.high_end + "] : " + value 
+			print "ival [" + root.low_end + "-" + root.high_end + "] : " + value + ", "
 			self.printTree(root.left)
 			self.printTree(root.right)
 
--- foam/src/foam/flowclashdetection/utils/store_trees_XML.py
+++ foam/src/foam/flowclashdetection/utils/store_trees_XML.py
@@ -1,2 +1,7 @@
 from lxml import etree
+from foam.flowclashdetection.mechanism import *
+
+root = etree.Element("root")
+
+root.append
 
